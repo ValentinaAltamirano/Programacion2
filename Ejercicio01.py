6c46cfe0e55de8bcb8bc19c25709a1adc1a01c3d
ValentinaAltamirano <107081335+ValentinaAltamirano@users.noreply.github.com>
2022-08-30 23:03:44 -0300

    Update Ejercicio01.py

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -5,53 +5,83 @@
 # mostrar(): Muestra los datos de la persona.
 # esMayorDeEdad(): Devuelve un valor lógico indicando si es mayor de edad.
 
-class Persona():
-    def __init__(self, nombre, edad, dni):
-        self.nombre = nombre;
-        self.edad = edad;
-        self.dni = dni;
+class Persona:
+    def __init__(self, nombre, dni, edad):
+        self.nombre = nombre
+        self.dni = dni
+        self.edad = edad
 
-    @property
+    def validarNombre(self, nombre):
+        if nombre.isnumeric():
+            nom = input("El nombre es invalido, ingrese el nombre nuevamente: ")
+            self.validarNombre(nom)
+        elif len(nombre) <= 2 and len(nombre) != 0:
+            nom = input("El nombre es invalido, ingrese el nombre nuevamente: ")
+            self.validarNombre(nom)
+        else:
+            self.setNombre(nombre)
+
+    def validarEdad(self, edad):
+        if edad.isnumeric():
+            if int(edad) >= 0 and int(edad) < 100:
+                self.setEdad(int(edad))
+            else:
+                edadN = input("La edad es invalida, ingrese la edad nuevamente: ")
+                self.validarEdad(edadN)
+        elif edad == "":
+            pass
+        else:
+            edadN = input("La edad es invalida, ingrese la edad nuevamente: ")
+            self.validarEdad(edadN)
+
+    def validarDni(self, dni):
+        if dni.isnumeric():
+            if len(dni) >= 6 and len(dni) <= 8:
+                self.setDni(int(dni))
+            else: 
+                dniN = input("El dni es invalido, ingrese el dni nuevamente: ")
+                self.validarDni(dniN)
+        elif dni == "":
+            pass
+        else:
+            dniN = input("El dni es invalido, ingrese el dni nuevamente: ")
+            self.validarDni(dniN)
+    
     def getNombre(self):
         return self.nombre
 
-    @property
+    def getDni(self):
+        return self.dni
+
     def getEdad(self):
         return self.edad
 
-    @property
-    def getDni(self):  
-        return self.dni
+    def setNombre(self,nombre):
+        self.nombre= nombre
     
-    def setNombre(self, nom):
-        self.nom = nom
-    
-    def setEdad(self, edad):
-        self.edad = edad
-    
-    def setDni(self, dni):
-        self.dni = dni
+    def setDni(self,dni):
+        self.dni= dni
 
-    def validarEdad(self):
-        return type(self.getEdad()) == int
-    
-    def esMayorDeEdad(self):
-        if self.validarEdad() == True:
-            if self.getEdad() >= 18:
-                print("True")
-            else:
-                print("False")
+    def setEdad(self,edad):
+        self.edad= edad
+
+    def mostrar(self):
+        print(f'Nombre: {self.getNombre()}, dni: {self.getDni()} edad: {self.getEdad()}')
+
+    def mayorEdad(self):
+        if self.edad == "":
+            pass
+        elif self.edad >= 18:
+            print("Es mayor de edad")
         else:
-            print("La edad ingresada es invalida")
+            print("Es menor de edad")
 
-    def __str__(self):
-        print(f'Nombre: {self.getNombre()} Edad: {self.getEdad()} Dni: {self.getDni()}')
-
-
-per1 = Persona('Jorge', 18, 123131)
-per1.__str__()
-per1.esMayorDeEdad()
-
-per2 = Persona('Sebatian', '18', 4324131)
-per2.__str__()
-per2.esMayorDeEdad()
+nom = input("Ingrese nombre: ")
+dni = input("Ingrese dni: ")
+edad = input("Ingrese edad: ")
+p1 = Persona(nom, dni, edad)
+p1.validarEdad(edad)
+p1.validarDni(dni)
+p1.validarNombre(nom)
+p1.mostrar()
+p1.mayorEdad()
